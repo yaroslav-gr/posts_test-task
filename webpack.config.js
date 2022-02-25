9a5e2f90f4eee43714c3ea33936da2a1ea47319d
yaroslav grigorev <yaroslav.purple@gmail.com>
2022-02-25 16:11:41 +0300

    install typescript, packages for typescript; ts, webpack, babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
@@ -15,16 +15,23 @@ module.exports = {
     module: {
         rules: [
         {
+            test: /\.(ts|tsx)$/,
+            exclude: /node_modules/,
+            use: {
+            loader: 'ts-loader',
+            },
+        },
+        {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: {
-            loader: 'babel-loader',
-            },
-        }
+              loader: 'babel-loader'
+            }
+          },
         ],
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
     devtool: 'source-map',
 };
